ca487fc0f15e2e6f9a109014abc023428586c1a2
FelixHommel <felixhommel04@gmail.com>
2026-06-05 18:22:26 +0200

    feat: add Ray class

---
+++ src/Ray.cuh
@@ -0,0 +1,24 @@
+#ifndef CRT_SRC_RAY_CUH
+#define CRT_SRC_RAY_CUH
+
+#include "src/Vec3.cuh"
+
+#include <cuda_runtime_api.h>
+
+/// \brief CUDA device class that can represent a euclidean vector.
+///
+/// \author Felix Hommel
+/// \date 6/5/2026
+struct Ray
+{
+public:
+    __device__ Ray() {} // NOLINT
+    __device__ Ray(const Vec3& origin, const Vec3& direction) : origin(origin), direction(direction) {}
+
+    __device__ Vec3 pointAtParameter(float t) const { return origin + (t * direction); }
+
+    Vec3 origin;
+    Vec3 direction;
+};
+
+#endif // !CRT_SRC_RAY_CUH
